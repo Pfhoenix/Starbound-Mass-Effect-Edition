f8934e80648832c4b68ccab6aede44de45daa403
Pfhoenix <pfhoenix@gmail.com>
2014-01-11 16:22:27 -0700

    Biotics, guns, script fixes
    Added biotic power : slam
    Added guns
    Fixed some script stuff

--- SMEE/tech/biotic-lift/biotic-lift.lua
+++ SMEE/tech/biotic-lift/biotic-lift.lua
@@ -1,41 +1,35 @@
-function init()
+function BioticLiftInit()
+	data.liftTime = tech.parameter("liftTime")
 	data.liftVelocity = tech.parameter("liftVelocity")
 	data.liftAcceleration = tech.parameter("liftAcceleration")
 	data.projIds = {}
 end
 
-function uninit()
+function BioticLiftUninit()
 	data.projIds = nil
 end
 
-function input(args)
-	if args.moves["special"] == 1 then
-		return "biotic-lift"
-	end
-	
-	return nil
-end
-
-function update(args)
-	local energyusage = 0
+-- takes args from the update function
+-- returns energy usage
+function BioticLiftUpdate(args)
+	-- if there's enough energy, fire a projectile
 	if args.actions["biotic-lift"] then
-		if tech.parameter("energyUsage") > args.availableEnergy then
-			return 0
+		if tech.parameter("energyUsage") <= args.availableEnergy then
+			local dir = {}
+			local pos = tech.position()
+			dir[1] = args.aimPosition[1] - pos[1]
+			dir[2] = args.aimPosition[2] - pos[2]
+			local dist = math.sqrt(dir[1] * dir[1] + dir[2] * dir[2])
+			local newpos = {}
+			newpos[1] = dir[1] / dist + pos[1]
+			newpos[2] = dir[2] / dist + pos[2]
+
+			local pid = world.spawnProjectile("biotic-lift", newpos, tech.parentEntityId(), dir, false)
+			local ppos = world.entityPosition(pid)
+			-- format is entityId of projectile, last position of the projectile, time to live for the lifting effect, and the list of lifted entities
+			table.insert(data.projIds, { id = pid, pos = ppos, ttl = data.liftTime, lifted = nil })
+			data.updateEnergyUsage = data.updateEnergyUsage + tech.parameter("energyUsage")
 		end
-		local dir = {}
-		local pos = tech.position()
-		dir[1] = args.aimPosition[1] - pos[1]
-		dir[2] = args.aimPosition[2] - pos[2]
-		local dist = math.sqrt(dir[1] * dir[1] + dir[2] * dir[2])
-		local newpos = {}
-		newpos[1] = dir[1] / dist + pos[1]
-		newpos[2] = dir[2] / dist + pos[2]
-		
-		local pid = world.spawnProjectile("biotic-lift", newpos, tech.parentEntityId(), dir, false)
-		local ppos = world.entityPosition(pid)
-		-- format is entityId of projectile, last position of the projectile, time to live for the lifting effect, and the list of lifted entities
-		table.insert(data.projIds, { id = pid, pos = ppos, ttl = 3, lifted = nil })
-		energyusage = tech.parameter("energyUsage")
 	end
 	
 	local cpos
@@ -60,17 +54,18 @@ function update(args)
 						if world.entityExists(e) then
 							stillgood = true
 							world.callScriptedEntity(e, "entity.setVelocity", {0, data.liftVelocity})
+							--world.spawnProjectile("singleDefaultBlue", world.entityPosition(e), tech.parentEntityId(), { 0, 0 }, false)
 						else table.remove(proj.lifted, l)
 						end
 					end
-					-- this projectile is no longer listing anything, so remove it
+					-- this projectile is no longer lifting anything, so remove it
 					if not stillgood then
 						table.remove(data.projIds, i)
 					end
 				end
 			else
 				-- use proj.pos to get enemies near the impact point
-				proj.lifted = world.entityQuery(proj.pos, 4, { withoutEntityId = tech.parentEntityId() } )
+				proj.lifted = world.entityQuery(proj.pos, 4, { withoutEntityId = tech.parentEntityId(), notAnObject = true } )
 				if not proj.lifted or #proj.lifted == 0 then
 					table.remove(data.projIds, i)
 				else
@@ -85,6 +80,28 @@ function update(args)
 			proj.pos = world.entityPosition(proj.id)
 		end
 	end
+end
+
+function init()
+	BioticLiftInit()
+end
+
+function uninit()
+	BioticLiftUninit()
+end
+
+function input(args)
+	if args.moves["special"] == 1 then
+		return "biotic-lift"
+	end
 	
-	return energyusage
+	return nil
+end
+
+function update(args)
+	data.updateEnergyUsage = 0
+	
+	BioticLiftUpdate(args)
+		
+	return data.updateEnergyUsage
 end
